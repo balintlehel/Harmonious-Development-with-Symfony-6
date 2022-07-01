447c15bee7a73afa9306f3919082d61283ba2b0e
Lehel_Balint <lehel.balint@verifone.com>
2022-07-01 15:51:02 +0200

    Wildcard routes

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homePage(): Response
+    {
+        return new Response('Title: PB jams');
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if ($slug) {
+            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+        } else {
+            $title = 'All genre';
+        }
+
+        return new Response($title);
+    }
+}
